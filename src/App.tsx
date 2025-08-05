fe46313907abba0a2186d44a71825a7b9e4a358d
yoshimitsut <shimitsutanaka@gmail.com>
2025-08-04 22:55:20 -0300

    correcao de erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ListOrder from './pages/ListOrder';
 import OrderCake from './pages/OrderCake';
 
